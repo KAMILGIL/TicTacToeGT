7db63a945815df935ce62a8ded6573e7d3ae5c4a
KAMILGIL <kamjx@users.noreply.github.com>
2020-09-19 18:22:20 +0300

    adf

---
+++ TicTacToeGTs/Assets/Scripts/SkinSelectorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinSelectorScript : MonoBehaviour
+{
+    public GameObject skin1;
+    public GameObject skin2;
+    public GameObject skin3;
+
+    private GameObject currentSkin; 
+
+    private void Start()
+    {
+        currentSkin = Instantiate(skin1, Vector3.zero, Quaternion.identity); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Destroy(currentSkin);
+            currentSkin = Instantiate(skin1, Vector3.zero, Quaternion.identity); 
+        } 
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Destroy(currentSkin);
+            currentSkin = Instantiate(skin2, Vector3.zero, Quaternion.identity);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Destroy(currentSkin);
+            currentSkin = Instantiate(skin3, Vector3.zero, Quaternion.identity); 
+        }
+    }
+}
