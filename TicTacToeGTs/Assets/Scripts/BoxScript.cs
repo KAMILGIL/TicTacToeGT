f9fb1d6142f2a304f239d9ea7f85d047ed84abf0
KAMILGIL <kamjx@users.noreply.github.com>
2020-09-20 21:16:04 +0300

    ;ljkj

---
+++ TicTacToeGTs/Assets/Scripts/BoxScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour
+{
+    private Material material;
+    private float changingSpeed = 0; 
+
+    private void Start()
+    {
+        this.material = GetComponent<MeshRenderer>().material; 
+    }
+
+    private void Update()
+    {
+        Color newColor = material.color;
+
+        newColor.a += changingSpeed * Time.deltaTime;
+        if (newColor.a >= 256)
+        {
+            newColor.a = 256;
+            changingSpeed = 0; 
+        } 
+        else if (newColor.a <= 0)
+        {
+            newColor.a = 0;
+            changingSpeed = 0; 
+        }
+
+        material.color = newColor; 
+    }
+
+    public void StartGlowing()
+    {
+        changingSpeed = 0.1f; 
+    } 
+
+    public void StartFading()
+    {
+        changingSpeed = -0.1f; 
+    } 
+}
